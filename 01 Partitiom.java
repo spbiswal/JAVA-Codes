a0c183d4dfa9a4be75b0b11f51bca56d39f108c1
Sai Prasad Biswal <96678586+spbiswal@users.noreply.github.com>
2022-05-17 13:12:33 +0530

    Update 01 Partitiom.java

--- 01 Partitiom.java
+++ 01 Partitiom.java
@@ -1,3 +1,5 @@
+/*Q.Given an array containing 0s and 1s. Write an algorithms to sort array so that 0s come first followed by 1s.*/
+
 import java.util.*;
 public class Partition01 {
 	public static void main(String[] args)
